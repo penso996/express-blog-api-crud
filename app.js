6c2ff77557ccf2f46bc6ec1201279496a2ef09a8
penso996 <penso.andrea@outlook.it>
2025-02-07 14:46:10 +0100

    updated main.js name

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // Calling express
 const express = require("express");
-const blog = express();
+const app = express();
 const port = 3000;
 
 // Defined static folder
-blog.use(express.static("public"));
+app.use(express.static("public"));
 
 // Defined home route
-blog.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Homepage");
 })
 
 // Importing main posts route
 const postsRouter = require("./routers/postsRouters.js");
 // Defined main posts route
-blog.use("/posts", postsRouter);
+app.use("/posts", postsRouter);
 
 
 // Starting server on specified port
-blog.listen(port, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
