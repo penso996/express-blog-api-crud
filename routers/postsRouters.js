1aa32d7596af4023f965744fd03a2411a9885c54
penso996 <penso.andrea@outlook.it>
2025-02-07 14:59:45 +0100

    fixed comments and naming in postsRouter.js

--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -1,35 +1,35 @@
 // Calling express for routing
 const express = require("express");
-const router = express.Router();
+const postsRouter = express.Router();
 
 // Importing controller functions
 const postsController = require("../controllers/postsController");
 
 // POSTS CRUD
 // index (imported)
-router.get("/", postsController.index);
+postsRouter.get("/", postsController.index);
 
 // show (imported)
-router.get("/:id", postsController.show);
+postsRouter.get("/:id", postsController.show);
 
 // store
-router.post("/", function (req, res) {
+postsRouter.post("/", function (req, res) {
     res.send("Create new posts");
 });
 
 // update
-router.put("/:id", function (req, res) {
-    res.send("Modify post " + req.params.id);
+postsRouter.put("/:id", function (req, res) {
+    res.send("Totally modify post " + req.params.id);
 });
 
 // modify
-router.patch("/:id", function (req, res) {
-    res.send("Modify post " + req.params.id);
+postsRouter.patch("/:id", function (req, res) {
+    res.send("Partially modify post " + req.params.id);
 });
 
 // destroy (imported)
-router.delete("/:id", postsController.destroy);
+postsRouter.delete("/:id", postsController.destroy);
 
 
 // Export router module
-module.exports = router;
+module.exports = postsRouter;
