6da538dd9202e436b7bbf525112edeb8d8eefc98
penso996 <penso.andrea@outlook.it>
2025-02-08 15:32:36 +0100

    fixed store function in postsController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -42,29 +42,28 @@ function show(req, res) {
 // Store function
 function store(req, res) {
 
-    // Retrieve last post object
-    const lastPost = postsData[postsData.length - 1];
-    // Retrive last post id
-    const idLastPost = lastPost.id;
-    // Create new post id
-    const newPostId = idLastPost + 1;
-    // Create new post object
+    // Retrieve the last post object to determine the next available ID
+    const lastPostId = postsData[postsData.length - 1];
+    // If there are posts, increment the last post's ID, otherwise set the ID to 1 (for the first post)
+    const newPostId = lastPostId ? lastPostId.id + 1 : 1;
+
+    // Create a new post object using data from the request body
     const newPost = {
         id: newPostId,
         title: req.body.title,
         content: req.body.content,
         image: req.body.image,
         tags: req.body.tags
-    }
+    };
 
-    // Add the new post in data_post
+    // Add the new post object to the postsData array
     postsData.push(newPost);
 
-    // Console log to check new data_post
+    // Send the response with a 201 status indicating that the post was created, and return the new post in JSON format
+    res.status(201).json(newPost);
+
+    // Log the updated postsData array to the console for debugging purposes
     console.log(postsData);
-    // Send new status and new post json
-    res.status(201);
-    res.json(newPost);
 };
 
 // Update function
