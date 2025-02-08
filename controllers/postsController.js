c93fb2286918f58dc0d59d8fb995bd44b9550d2b
penso996 <penso.andrea@outlook.it>
2025-02-08 15:37:16 +0100

    fixed update function in postsController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -69,31 +69,32 @@ function store(req, res) {
 // Update function
 function update(req, res) {
 
-    // Filter by id
+    // Retrieve the post ID from the request parameters and parse it as an integer
     const id = parseInt(req.params.id);
+
+    // Find the post in postsData by comparing IDs
     const post = postsData.find(post => post.id === id);
 
     // If no element found by id
     if (!post) {
         // Error 404
-        res.status(404);
-        return res.json({
+        return res.status(404).json({
             error: "Not found",
             message: "Post not found"
         });
     }
 
-    // Modify whole post
+    // If post is found, modify the post's properties with data from the request body
     post.title = req.body.title;
     post.content = req.body.content;
     post.image = req.body.image;
     post.tags = req.body.tags;
 
-    // Console log to check new data_post
-    console.log(postsData);
+    // Return the updated post as a JSON response
+    res.json(post);
 
-    // Send new modified post json
-    res.json(postsData[id - 1]);
+    // Log the updated postsData to the console for debugging purposes
+    console.log(postsData);
 };
 
 // Modify
